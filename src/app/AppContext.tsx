48bbb7e74c3111e141c2b1ef076da2e8a63366b3
Eric Frye <eric.frye25@gmail.com>
2025-10-12 12:16:24 -0400

    remove unused filters

--- src/app/AppContext.tsx
+++ src/app/AppContext.tsx
@@ -7,14 +7,8 @@ import {Option} from 'react-multi-select-component'
 const appData = {
     cardPredictions: [] as CardPrediction[],
     setCardPredictions: (_: CardPrediction[]) => {},
-    selectedTier: [] as Option[],
-    setSelectedTier: (_: Option[]) => {},
     selectedTeam: [] as Option[],
     setSelectedTeam: (_: Option[]) => {},
-    selectedDivision: [] as Option[],
-    setSelectedDivision: (_: Option[]) => {},
-    selectedLeague: [] as Option[],
-    setSelectedLeague: (_: Option[]) => {},
     cardPage: {
         CurrentPage: 1,
         PageSize: 10
@@ -39,23 +33,14 @@ export default function AppProvider({
     const currentLiveUpdateID = parseInt(process.env.NEXT_PUBLIC_CURRENTLIVEUPDATEID ?? "0")
     
     const [cardPredictions, setCardPredictions] = React.useState([] as CardPrediction[])
-    const [selectedTier,setSelectedTier] = React.useState([] as Option[]);
     const [selectedTeam,setSelectedTeam] = React.useState([] as Option[]);
-    const [selectedDivision,setSelectedDivision] = React.useState([] as Option[]);
-    const [selectedLeague,setSelectedLeague] = React.useState([] as Option[]);
     const [cardPage, setCardPage] = React.useState(cardPagination);
 
     const appData = {
         cardPredictions,
         setCardPredictions,
-        selectedTier,
-        setSelectedTier,
         selectedTeam,
         setSelectedTeam,
-        selectedDivision,
-        setSelectedDivision,
-        selectedLeague,
-        setSelectedLeague,
         cardPage,
         setCardPage,
         currentLiveUpdateID,
--- src/app/PtCardListFilter.tsx
+++ src/app/PtCardListFilter.tsx
@@ -1,78 +1,33 @@
 import * as React from 'react';
 import { MultiSelect } from 'react-multi-select-component';
-import { Tier } from '../types'
+import { Team } from '../types'
 import { AppContext } from "./AppContext";
 
 export const PtCardListFilter = () => {
 
-    const { selectedTier, setSelectedTier, selectedTeam, setSelectedTeam, selectedDivision, setSelectedDivision, selectedLeague, setSelectedLeague } = React.useContext(AppContext);
-    
-    const tiers = [
-        {label:"Iron", value:Tier.Iron},
-        {label:"Bronze", value:Tier.Bronze},
-        {label:"Silver", value:Tier.Silver},
-        {label:"Gold", value:Tier.Gold},
-        {label:"Diamond", value:Tier.Diamond},
-        {label:"Perfect", value:Tier.Perfect}
-    ];
+    const { selectedTeam, setSelectedTeam } = React.useContext(AppContext);
 
-    const teams = [
-        {label:'Philadelphia Phillies', value: 'Philadelphia Phillies'},
-        {label:'Atlanta Braves', value: 'Atlanta Braves'},
-        {label:'Los Angeles Dodgers', value: 'Los Angeles Dodgers'},
-        {label:'New York Yankees', value: 'New York Yankees'},
-        {label:'Cleveland Indians', value: 'Cleveland Indians'},
-        {label:'Seattle Mariners', value: 'Seattle Mariners'},
-    ]
-
-    const divisions = [
-        {label:"NL East", value: 0},
-        {label:"AL East", value: 1},
-        {label:"AL Central", value: 2},
-    ]
-
-    const leagues = [
-        {label:"National League", value: 0},
-        {label:"American League", value: 1},
-    ]
+    const teams = [...Array(Object.keys(Team).length).keys()].map((enumIndex) => {
+        return {
+            label: Team[enumIndex],
+            value: Team[enumIndex],
+        }
+    })
 
     return (
         <div>
             <div className="mb-2">
-                <MultiSelect
-                    options={tiers}
-                    value={selectedTier}
-                    onChange={setSelectedTier}
-                    labelledBy="Select"
-                    hasSelectAll={false}
-                />
-            </div>
-            <div className="mb-2">
-                <MultiSelect
-                    options={teams}
-                    value={selectedTeam}
-                    onChange={setSelectedTeam}
-                    labelledBy="Select"
-                    hasSelectAll={false}
-                />
-            </div>
-            <div className="mb-2">
-                <MultiSelect
-                    options={divisions}
-                    value={selectedDivision}
-                    onChange={setSelectedDivision}
-                    labelledBy="Select"
-                    hasSelectAll={false}
-                />
-            </div>
-            <div className="mb-2">
-                <MultiSelect
-                    options={leagues}
-                    value={selectedLeague}
-                    onChange={setSelectedLeague}
-                    labelledBy="Select"
-                    hasSelectAll={false}
-                />
+                <div>Team</div>
+                <div className="w-1/5">
+                    <MultiSelect
+                        options={teams}
+                        value={selectedTeam}
+                        onChange={setSelectedTeam}
+                        labelledBy="Select"
+                        hasSelectAll={false}
+                        
+                    />
+                </div>
             </div>
         </div>
     )
